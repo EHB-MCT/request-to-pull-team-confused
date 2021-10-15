7e9f733a75e249dd7741d245ae595775a8db4da9
lukacrabbe <luka.crabbe@student.ehb.be>
2021-10-15 11:20:22 +0200

    feat(socket):added full socket functionality

--- index.js
+++ index.js
@@ -5,13 +5,31 @@ const server=http.createServer(app);
 const {Server}=require("socket.io");
 const io=new Server(server);
 
+const messages=[];
+
 app.get('/',(req,res)=>{
     res.send("Hello World");
 });
 
-app.get('/messages',(req,res)=>{
+app.get('/socket',(req,res)=>{
     res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
+      socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        messages.push(msg);
+        console.log(messages);
+      });
+      socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+      });
+      
+  });
 
 server.listen(3000,()=>{
     console.log('listening on port 3000');
