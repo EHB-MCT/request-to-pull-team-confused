56903e99bf494c2092aa220a85ae9dd399b54c93
lukacrabbe <luka.crabbe@student.ehb.be>
2021-10-15 11:01:47 +0200

    feat(socket setup):socket endpoint

---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require("express");
+const http=require("http");
+const app=express();
+const server=http.createServer(app);
+const {Server}=require("socket.io");
+const io=new Server(server);
+
+app.get('/',(req,res)=>{
+    res.send("Hello World");
+});
+
+app.get('/messages',(req,res)=>{
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(3000,()=>{
+    console.log('listening on port 3000');
+});
