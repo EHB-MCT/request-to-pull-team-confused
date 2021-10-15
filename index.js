12ddcd6f7a345accaabac7aa637f73515d0bd213
JulienCleys <cleysjulien@gmail.com>
2021-10-15 11:17:46 +0200

    aanmaking van array

---
+++ index.js
@@ -0,0 +1,19 @@
+// const data = [{
+//     "Name": "Cleys",
+//     "Surname": "Julien",
+//     "Age": 22
+// }, {
+//     "Name": "Clement",
+//     "Surname": "Kevin",
+//     "Age": 23
+// }, {
+//     "Name": "Crabbe",
+//     "Surname": "Luka",
+//     "Age": 24
+// }, {
+//     "Name": "Lanckriet",
+//     "Surname": "Kevin",
+//     "Age": 24
+// }];
+
+const data = [];
